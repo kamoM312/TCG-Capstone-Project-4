8c9a0f987d2363d4bc7520c4c7c1d29a21d09ba5
kamo312 <kamogelomalema1@gmail.com>
2025-05-30 15:18:00 -0400

    menu.html - menu item click functionality

---
+++ public/app.js
@@ -0,0 +1,26 @@
+const menuItem = document.getElementsByClassName("menu-item");
+console.log(menuItem);
+
+let index = 0;
+
+function currentImage() {
+    index = parseInt(this.getAttribute("data-index"));
+    // console.log(parseInt(this.getAttribute("data-index")));
+    let imageLocation = menuItem[index].children[0].children[0].getAttribute("src");
+    let itemName = menuItem[index].children[1].children[0].innerText;
+    // let itemDescription = menuItem[index].children[1].children[0].innerText;
+    // console.log(itemDescription);
+    // let itemDescription = menuItem[index].children[0].children[0].getAttribute("src");
+    // console.log(imageLocation);
+    // console.log(document.querySelector(""))
+    document.querySelector("#menu-modal").style.backgroundImage = `url('${imageLocation}')`;
+    document.querySelector(".item-name").innerText = itemName;
+    document.querySelector(".item-description").innerText = itemDescription;
+    document.querySelector('#menu-modal').classList.remove("hidden");
+}
+
+// make click event work on every gallery item
+for( i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener("click", currentImage);
+}
+
